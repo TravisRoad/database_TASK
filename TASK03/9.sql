7beb70a1f4c6876e2319480776db2049494e1fa0
Travis <lxy_montage@163.com>
2020-11-22 19:42:42 +0800

    UPDATE 9.SQL

--- TASK03/9.sql
+++ TASK03/9.sql
@@ -1,19 +1,15 @@
 SELECT
-  ONE.SECTOR_ID,
-  SECTOR_NAME,
-  NCELL_ID,
-  NSECTOR_ID
+  *
 FROM
   (
-    (
       select
-        *
+        A.SECTOR_ID,C.SECTOR_NAME,A.NCELL_ID,B.NSECTOR_ID
       FROM
         user58db.`7tbatuc2i` AS A
-        INNER JOIN user58db.`8tbatuhandover` AS B ON A.SECTOR_ID = B.NSECTOR_ID
+        INNER JOIN user58db.`8tbatuhandover` AS B
+        INNER JOIN user58db.`1tbcell` AS C
+        ON A.SECTOR_ID = B.NSECTOR_ID AND A.SECTOR_ID = C.SECTOR_ID
       WHERE
-        SECTOR_ID = "238397-1"
+        A.SECTOR_ID = "238397-1"
         and COSITE = 1
-    ) AS TMP
-    INNER JOIN user58db.`1tbcell` AS ONE ON TMP.SECTOR_ID = ONE.SECTOR_ID
-  );
+    ) AS TMP;
