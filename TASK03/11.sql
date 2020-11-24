4877e8e0f76787b535d44406e551c027974fc3ea
Travis <lxy_montage@163.com>
2020-11-25 02:11:05 +0800

    11 done

--- TASK03/11.sql
+++ TASK03/11.sql
@@ -1,12 +1,33 @@
-SELECT MAX(A.Traffic), MIN(A.Traffic), AVG(A.Traffic)
-FROM user58db.tbcell_traffic AS A
-	     INNER JOIN user58db.`1tbcell` AS B
-	                ON A.Sector_ID = B.SECTOR_ID
-WHERE (TIME(A.Date) BETWEEN "09:00" AND "11:00" OR
-       TIME(A.Date) BETWEEN "19:00" AND "21:00")
-  AND B.LATITUDE BETWEEN 0 AND 90
-  AND B.LONGITUDE BETWEEN 100 AND 120
-  AND DATE(A.Date) BETWEEN "01/05/2020" AND "31/05/2020"
-GROUP BY A.SECTOR_ID
+-- 最大值得相关信息
+with t as(SELECT sum(A.Traffic) as s, A.Sector_ID, B.SECTOR_NAME, B.LONGITUDE, B.LATITUDE
+      FROM user58db.`tbcell_traffic` AS A
+               INNER JOIN user58db.`1tbcell` AS B ON A.Sector_ID = B.SECTOR_ID
+      WHERE (
+          A.Hour BETWEEN 9 AND 11 OR A.Hour BETWEEN 19 AND 21
+          )
+        AND B.LATITUDE BETWEEN -90
+          AND 90
+        AND B.LONGITUDE BETWEEN 0
+          AND 120
+        AND str_to_date(A.Date, "%m/%d/%Y %H:%i:%s") BETWEEN str_to_date("05/01/2020", "%m/%d/%Y %H:%i:%s")
+          AND str_to_date("5/31/2020", "%m/%d/%Y %H:%i:%s")
+      GROUP BY A.SECTOR_ID)
+select max(s) as MAX,t.Sector_ID, t.SECTOR_NAME, t.LATITUDE, t.LONGITUDE
+from t
+WHERE s = (SELECT MAX(t.s) from t);
 
--- TODO
+-- 最大最小平均
+with t as(SELECT sum(A.Traffic) as s, A.Sector_ID, B.SECTOR_NAME, B.LONGITUDE, B.LATITUDE
+      FROM user58db.`tbcell_traffic` AS A
+               INNER JOIN user58db.`1tbcell` AS B ON A.Sector_ID = B.SECTOR_ID
+      WHERE (
+          A.Hour BETWEEN 9 AND 11 OR A.Hour BETWEEN 19 AND 21
+          )
+        AND B.LATITUDE BETWEEN -90
+          AND 90
+        AND B.LONGITUDE BETWEEN 0
+          AND 120
+        AND str_to_date(A.Date, "%m/%d/%Y %H:%i:%s") BETWEEN str_to_date("05/01/2020", "%m/%d/%Y %H:%i:%s")
+          AND str_to_date("5/31/2020", "%m/%d/%Y %H:%i:%s")
+      GROUP BY A.SECTOR_ID)
+SELECT MAX(t.s),MIN(t.s),AVG(t.s) from t;
