9b804aa81ce4838ac741a67c5c076f12aa4c8a71
Travis <lxy_montage@163.com>
2020-11-25 11:55:55 +0800

    28 done

---
+++ TASK03/28.sql
@@ -0,0 +1,7 @@
+update user58db.`7tbatuc2i`
+set RATIO_ALL = case
+    when (`RANK` >= 1 and COSITE = 1) then RATIO_ALL - 1
+    when COSITE != 1 then RATIO_ALL + 1
+    else RATIO_ALL
+end
+where SECTOR_ID = "238397-1";
