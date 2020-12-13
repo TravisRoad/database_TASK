b5c9493c7cc4bc453ffcaf78700a285926e218d3
Travis <lxy_montage@163.com>
2020-12-13 23:42:24 +0800

    new

--- TASK03/17_1.sql
+++ TASK03/17_1.sql
@@ -5,5 +5,3 @@ from `11tbhandover`
 where NCELL not in (select NCELL
                     from `11tbhandover`
                     where NCELL in ('15290-128', '259595-1', '124711-0', '47444-1'))
-
-
